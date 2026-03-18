fc0dbf2e16902bc0ccad6861895613bf2d19e24f
Spectronyx <pt.rj578@gmail.com>
2026-03-18 19:17:21 +0530

    changed the frontend urls

--- server/server.js
+++ server/server.js
@@ -6,15 +6,13 @@ import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import path from "path";
-import { fileURLToPath } from "url";
 
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
 // Fix for __dirname in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 const allowedOrigins = [
     "http://localhost:5173",
     "https://authentication-system-080k.onrender.com",
@@ -32,12 +30,10 @@ app.use(
 );
 
 //API ENDPOINTS
+
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 
-app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
